5accd16113f97a34acc0712d5428b10ea1dfdc0c
Stevert Lobo <stevertlobo@gmail.com>
2019-07-16 23:18:07 +0530

    Comment section added in blog module

---
+++ app/src/main/java/CommentsRecyclerAdapter.java
@@ -0,0 +1,108 @@
+package com.example.ishmaamin.curatech;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
+public class CommentsRecyclerAdapter extends RecyclerView.Adapter<CommentsRecyclerAdapter.ViewHolder> {
+
+    public List<Comments> commentsList;
+    public Context context;
+    private FirebaseFirestore firebaseFirestore;
+
+    public CommentsRecyclerAdapter(List<Comments> commentsList){
+
+        this.commentsList = commentsList;
+
+    }
+
+    @Override
+    public CommentsRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.comment_list_item, parent, false);
+        context = parent.getContext();
+        return new CommentsRecyclerAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final CommentsRecyclerAdapter.ViewHolder holder, int position) {
+
+        holder.setIsRecyclable(false);
+
+        String commentMessage = commentsList.get(position).getMessage();
+        final String commentUsername=commentsList.get(position).getUser_id();
+        holder.setComment_message(commentMessage);
+
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseFirestore.collection("Users").document(commentUsername).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+
+                if (task.isSuccessful()){
+
+                    String username=task.getResult().getString("name");
+                    holder.setUsername(username);
+                }else{
+
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+
+        if(commentsList != null) {
+
+            return commentsList.size();
+
+        } else {
+
+            return 0;
+
+        }
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private View mView;
+
+        private TextView comment_message;
+        private TextView comment_username;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setComment_message(String message){
+
+            comment_message = mView.findViewById(R.id.comment_message);
+            comment_message.setText(message);
+
+        }
+
+        public void setUsername(String username){
+            comment_username =mView.findViewById(R.id.comment_username);
+            comment_username.setText(username);
+        }
+
+    }
+
+}
