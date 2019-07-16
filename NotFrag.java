47bbbf38761077f6590f586b6c645c78d1cfdc1b
IshmaAmin <37192009+IshmaAmin@users.noreply.github.com>
2019-07-17 00:10:20 +0530

    Fragment View Activity

---
+++ NotFrag.java
@@ -0,0 +1,29 @@
+package com.example.ishmaamin.curatech;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NotFrag extends Fragment {
+
+
+    public NotFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_not, container, false);
+    }
+
+}
